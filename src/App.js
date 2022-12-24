6bc847d5c153606d4bd12400cbda00ade9e1d29a
khantlinnyan <khantlinnyan011027@gmail.com>
2022-12-24 22:06:24 +0630

    first commit useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  let num = 0;
+  const [count, setCount] = useState(num);
+  function increase() {
+    setCount((num)=> num + 1);
+    setCount((num)=> num + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList/>
+      <p className="text-red600 text-lg mt-5">{count}</p>
+      <button className="text-lg text-bold bg-red-600 rounded-sm p-2" onClick={increase}>Add num</button>
+      
     </div>
   );
+}
+const cards = [
+  {
+  id: 1,
+  image: "https://via.placeholder.com/150/92c952",
+  title: "Card Title Two",
+  description: "Hey8"
+ }
+,{
+  id: 2,
+  image: "https://via.placeholder.com/150/771796",
+  title: "Card Title Two",
+  description: "Hey2"
+ }
+,{
+  id: 3,
+  image: "https://via.placeholder.com/150/771796",
+  title: "Card Title Three",
+  description: "Hey3"
+ }];
+function CardList() {
+  return <main className='card-list'>
+  {cards.map((card)=> {
+    const {image, title, description} = card
+    return <Card key={card.id} {...card}/>
+  })}
+    
+  </main>
+}
+function Card(props) {
+  const {image , title, description} = props;
+  return (
+    <section className='card'>
+      <img src={image}/>
+      <h1 className="text-3xl text-" style={{marginTop: "1rem"}}>{title}</h1>
+      <p className='text-lg'>{description}</p>
+    </section>
+  )
 }
 
 export default App;
